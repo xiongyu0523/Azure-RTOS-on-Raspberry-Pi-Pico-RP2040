e4762cadd476772697ac3856940fd99cf181421b
neo <xiongyu0523@gmail.com>
2021-03-02 10:25:57 +0800

    first working demo

--- test.c
+++ test.c
@@ -6,12 +6,28 @@
 
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "pico/binary_info.h"
+
+extern void demo_threadx(void);
+extern void __vectors(void);
+extern void isr_pendsv(void);
+extern void isr_systick(void);
+extern void _tx_initialize_low_level(void);
 
 int main() {
+
     stdio_init_all();
-    while (true) {
-        printf("Hello, neo!\n");
-        sleep_ms(1000);
-    }
+    printf("ThreadX Demo\n");
+
+    _tx_initialize_low_level();
+
+    // LED OFF
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+    gpio_put(PICO_DEFAULT_LED_PIN, 0);
+
+    demo_threadx();
+
     return 0;
 }
